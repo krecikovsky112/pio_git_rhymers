d833d86d5c9f2a2e6eba2ec7449d714365eaa4ee
krecikovsky112 <krecikovsky112@gmail.com>
2020-12-16 16:07:13 +0100

    refresh code to default

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,50 +2,35 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int SIZE = 12;
-    private static final int ERROR = -1;
-    private final int[] numbers = new int[getSIZE()];
+    public static final int SIZE = 12;
+    public static final int ERROR = -1;
+    private final int[] numbers = new int[SIZE];
 
-    private int total = getERROR();
-
-    private static int getSIZE() {
-        return SIZE;
-    }
-
-    private static int getERROR() {
-        return ERROR;
-    }
+    public int total = ERROR;
 
     public void countIn(int in) {
         if (!isFull())
-            getNumbers()[++total] = in;
+            numbers[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == getERROR();
+        return total == ERROR;
     }
 
     public boolean isFull() {
-        return total == getSIZE() -1;
-    }
-
-    public int getTotal() {
-        return total;
+        return total == SIZE-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return getERROR();
-        return getNumbers()[total];
+            return ERROR;
+        return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return getERROR();
-        return getNumbers()[total--];
+            return ERROR;
+        return numbers[total--];
     }
 
-    private int[] getNumbers() {
-        return numbers;
-    }
 }
